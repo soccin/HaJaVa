4a88cfc33d022454828f0a8ef3b481c9e8a92977
Nicholas D. Socci <socci@lux>
2012-11-25 23:17:28 -0500

    Added hMYC chromosome

--- bin/clipAdapters.sh
+++ bin/clipAdapters.sh
@@ -25,9 +25,9 @@ echo $ADAPTER_2
 #   casava 1.8 ==> xxxx:xxxx:xxxx 1:N:1
 #
 qsub -pe alloc 2 -N $TAG $QCMD \
-  zcat $FASTQ1 \| head -40000 \| fastx_clipper -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq \> ${OUT1}__CLIP.log
+  zcat $FASTQ1 \| head -160000 \| fastx_clipper -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq \> ${OUT1}__CLIP.log
 qsub -pe alloc 2 -N $TAG $QCMD \
-  zcat $FASTQ2 \| head -40000 \| fastx_clipper -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq \> ${OUT2}__CLIP.log
+  zcat $FASTQ2 \| head -160000 \| fastx_clipper -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq \> ${OUT2}__CLIP.log
 $QSYNC $TAG
 echo "Done with clipping...rePairing..."
 bin/matchPE.py ${OUT1}__TMP.fastq ${OUT2}__TMP.fastq ${OUT1} ${OUT2}
