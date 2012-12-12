db456718af643965f83c1805a7fad4317fad8f57
Nicholas D. Socci <socci@lux>
2012-12-11 21:20:04 -0500

    Added hMYC mouse

--- data/dataPaths.sh
+++ data/dataPaths.sh
@@ -8,6 +8,12 @@
 #    GENOME_BWA must point to the BWA index
 
 GDIR=/ifs/data/bio/Genomes/M.musculus/mm9
-GENOME_FASTQ=$GDIR/mouse_mm9__All.fa
-GENOME_BWA=$GDIR/BWA/DNA/mouse_mm9__All.fa
+
+#WT MOUSE
+#GENOME_FASTQ=$GDIR/mouse_mm9__All.fa
+#GENOME_BWA=$GDIR/BWA/DNA/mouse_mm9__All.fa
+
+#hMYC MOUSE
+GENOME_FASTQ=$GDIR/mouse_mm9__All_hMYC.fa
+GENOME_BWA=$GDIR/BWA/DNA/mouse_mm9__All_hMYC.fa
 
