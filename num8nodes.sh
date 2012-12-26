fdb37ae89c6934d518d6ad61680fd861034cd7b5
Nicholas D. Socci <socci@lux>
2012-12-26 13:41:44 -0500

    Return True of no jobs running

--- num8nodes.sh
+++ num8nodes.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-qstat -f| fgrep BIP | egrep -v "d$" | fgrep all.q | awk '{print $3}' | awk -F"/" '$2<=4{print $2}' | wc -l
+
+ISEMPTY=$(qstat)
+
+if [ -z "$ISEMPTY" ]; then
+    echo "1"
+else
+    qstat -f| fgrep BIP | egrep -v "d$" | fgrep all.q | awk '{print $3}' | awk -F"/" '$2<=4{print $2}' | wc -l
+fi
