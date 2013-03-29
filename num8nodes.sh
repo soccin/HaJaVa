e8209adbdf38a0b5e85ae195442026235d7418af
Nicholas D. Socci <socci@rho.mskcc.org>
2013-03-29 11:19:27 -0400

    Removed more SGE dependent stuff

--- num8nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ISEMPTY=$(qstat)
-
-if [ -z "$ISEMPTY" ]; then
-    echo "1"
-else
-    qstat -f| fgrep BIP | egrep -v "d$" | fgrep all.q | awk '{print $3}' | awk -F"/" '$2<=4{print $2}' | wc -l
-fi
