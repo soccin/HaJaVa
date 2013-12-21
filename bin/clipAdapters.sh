b647a68fe49b50851cd7cb4ddd286c8cbb7c51fe
Nicholas D. Socci <socci@lux>
2013-12-21 01:29:29 -0500

    Reads from config.sh in CWD for Debug readlimit

--- bin/clipAdapters.sh
+++ bin/clipAdapters.sh
@@ -18,8 +18,25 @@ echo $OUT1 $OUT2
 echo $ADAPTER_1
 echo $ADAPTER_2
 
-gzcat $FASTQ1 | $HJV_ROOT/bin/fastx_clipper -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
-gzcat $FASTQ2 | $HJV_ROOT/bin/fastx_clipper -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+if [ -e config.sh ]; then
+    echo "sourceing config.sh"
+    source config.sh
+fi
+
+MIN_ADAPTER_LEN=8
+
+if [ "$HJV_READLIMIT" != "" ]; then
+    echo HJV_READLIMIT=[$HJV_READLIMIT]
+    gzcat $FASTQ1 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
+    gzcat $FASTQ2 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+else
+    gzcat $FASTQ1 | \
+        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
+    gzcat $FASTQ2 | \
+        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+fi
+
+#echo "BREAK::clipAdapters Line 37"; exit
 
 echo "Done with clipping...rePairing..."
 $SDIR/matchPE.py ${OUT1}__TMP.fastq ${OUT2}__TMP.fastq ${OUT1} ${OUT2}
