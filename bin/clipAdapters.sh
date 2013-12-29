4a2943f6e9685abf71107d80709c8574cbb70f2a
Nicholas D. Socci <socci@lux>
2013-12-29 14:50:42 -0500

    Changed back to default clipping

--- bin/clipAdapters.sh
+++ bin/clipAdapters.sh
@@ -23,17 +23,26 @@ if [ -e config.sh ]; then
     source config.sh
 fi
 
-MIN_ADAPTER_LEN=8
+#MIN_ADAPTER_LEN=8
+#if [ "$HJV_READLIMIT" != "" ]; then
+#    echo HJV_READLIMIT=[$HJV_READLIMIT]
+#    gzcat $FASTQ1 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
+#    gzcat $FASTQ2 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+#else
+#    gzcat $FASTQ1 | \
+#        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
+#    gzcat $FASTQ2 | \
+#        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+#fi
 
+# Old default clipping
 if [ "$HJV_READLIMIT" != "" ]; then
     echo HJV_READLIMIT=[$HJV_READLIMIT]
-    gzcat $FASTQ1 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
-    gzcat $FASTQ2 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+    gzcat $FASTQ1 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
+    gzcat $FASTQ2 | head -$HJV_READLIMIT | $HJV_ROOT/bin/fastx_clipper -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
 else
-    gzcat $FASTQ1 | \
-        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_1 -o ${OUT1}__TMP.fastq > ${OUT1}__CLIP.log
-    gzcat $FASTQ2 | \
-        $HJV_ROOT/bin/fastx_clipper -M $MIN_ADAPTER_LEN -Q33 -v -n -a $ADAPTER_2 -o ${OUT2}__TMP.fastq > ${OUT2}__CLIP.log
+	echo "DEBUGGING CODE"
+	echo "MUST HAVE HJV_READLIMIT SET"
 fi
 
 #echo "BREAK::clipAdapters Line 37"; exit
